41a1c15960e9e69aaa932aa4f68afeb15d238e71
sigfrido <sigfrido@linux-rppb.site>
2015-06-27 20:00:56 -0500

    Se formateo el código

--- src/reves/Reves.java
+++ src/reves/Reves.java
@@ -15,16 +15,15 @@ public class Reves {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    char [] hola= new char[5];
-    hola[0]='H';
-    hola[1]='O';
-    hola[2]='L';
-    hola[3]='A';
-    hola[4]='A';
-    for(int i =3; i< hola.length; i--){
-     System.out.println(hola[i]);
-     System.out.print(i);
+        char[] hola = new char[5];
+        hola[0] = 'H';
+        hola[1] = 'O';
+        hola[2] = 'L';
+        hola[3] = 'A';
+        hola[4] = 'A';
+        for (int i = 3; i < hola.length; i--) {
+            System.out.println(hola[i]);
+            System.out.print(i);
+        }
     }
-    }
-    }
-    
+}
